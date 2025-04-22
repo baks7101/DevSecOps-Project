9ac8f73429aade4fa13c720773e185dcaad329a7
Bakary <bsillah15@gmail.com>
2025-04-22 21:25:00 +0100

    update

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ provider "aws" {
 resource "aws_s3_bucket" "terraform-today1"{
     bucket = "terraform-today1"
 }
+terraform {
+  backend "s3" {
+    #replace this with your bucket name!
+    bucket        = "terraform-today1"
+    key           = "global/s3/terraform.tfstate"
+    region        = "eu-west-2"
+  }
+}
